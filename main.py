130f94e88eec1a85469fac6475a87ed8b8e52ba3
akshat <twisted.nematic57@gmail.com>
2024-11-23 21:39:54 -0800

    Update main.pygabort?

--- main.py
+++ main.py
@@ -5,30 +5,29 @@ print("INIT: boot")
 machine.freq(250000000)
 led = machine.Pin("LED", machine.Pin.OUT)
 led.on()
+a = 0
 
-red = machine.Pin(14, mode=machine.Pin.IN, pull=machine.Pin.PULL_UP)
-white = machine.Pin(15, mode=machine.Pin.IN, pull=machine.Pin.PULL_UP)
-print("Set to in")
-red = machine.Pin(14, mode=machine.Pin.OUT, pull=machine.Pin.PULL_UP)
-white = machine.Pin(15, mode=machine.Pin.OUT, pull=machine.Pin.PULL_UP)
-print("Set to out")
-
-def put(bit):
-    if bit is 1:
-        red = machine.Pin(14, mode=machine.Pin.IN, pull=machine.Pin.PULL_UP)
-        
 def red_0():
     red = machine.Pin(14, mode=machine.Pin.OUT, pull=machine.Pin.PULL_UP)
-    red.on()
+    red.off()
     
 def red_1():
     red = machine.Pin(14, mode=machine.Pin.IN, pull=machine.Pin.PULL_UP)
     #red.on()
     
 def white_0():
-    red = machine.Pin(15, mode=machine.Pin.OUT, pull=machine.Pin.PULL_UP)
-    red.on()
+    white = machine.Pin(15, mode=machine.Pin.OUT, pull=machine.Pin.PULL_UP)
+    white.off()
     
 def white_1():
-    red = machine.Pin(15, mode=machine.Pin.IN, pull=machine.Pin.PULL_UP)
-    #red.on()
+    white = machine.Pin(15, mode=machine.Pin.IN, pull=machine.Pin.PULL_UP)
+    #white.on()
+
+
+
+def put(bit):
+    if bit == 1:
+        red_0()
+        while white == 1:
+            a += 1
+        xyz = 3
